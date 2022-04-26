6a0795b7e402185cd1ca4a2d15324929124ca99f
Giokabaneri <58143000+IMpusing@users.noreply.github.com>
2022-04-26 23:31:28 +0700

    Update Profile

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://previews.dropbox.com/p/thumb/ABhH0WDNBfuvKJST-R5vU-gpxWGbN6VTNP8aS5HDFmijMC0oiT-WslwRCc2N-7LzPCBYdCi1xE1GpKAVSQsk8WBFveV2NfM5bOfN4W3GgGaVSkGW1W0CWUjUjVVx61-15SRyRkSUjN536WlW_K1LfsV96G8KizVqD3pIflA80hOxskihAs_FspHuYQxqXPhgDdM7ziEwmD-2tF4xhhqYsLixgVDksGrXyl7Gilvcvb02DpcCFmQMEOLwYAY7Wf9df8eAzOObe_X9tmXe9G6-y0VcVV5fnWT-R8--ov1o_otsSZNyCBlabkf6vV-Y3CzlwsbQbNAnDYcQjooXxSjtRdqpZqxCCR8cOGtrGYnt7cbNqvB59_b3OeeHFgzjpGAiTj3Yt2S1hILGn0lBRKfmr-5CBtsX-I6W52cHVO1NbRXMS-HxdsXVlTQPXGJJYYPvqW4gMB4fh_Dl0qn7kuOPzjLRWGVHcikq5JQvhj9D3G_Ye5yhYs9CKEWIT0x-ZWATh951btBvREHyHijXEkBgnEJ_fFkc9ruJX5jCVc6v7-tglwIurpC_i6H8xZXpJUofDa_1G1FV4rFepIWxENQYZGlpbW9M5mXQyGIY7PvsqQ9CBtNwnu7i_W2W_aXVeHlOhU14z41RnlwxVI9nObefJ0aVlQ_NVKgcSr_Rjt6Qy8zztDPinxvOSsfTQtdjoQyRWmk566C0DR51J_dnUdWC1DKD8VU7a0F9EW0Z2zCfeHsrZjmaKW7Ee9J4xWdC-CoNn3rfCc4GLiYJv5GLDI7d-wKr51a4J_S-E7t96pfr6tpbGyhIHRV0yLMpjLt5fLGyd9rXxkm2yoqM9MPSGUNZWpUxDqxivGbYB2Ey5Sxh6xSgb2Un6HSuwE7-8lAL4ZKOAo8yiWwiA9LKeDhkJV-At3wdcaMJFd7ihNz4kc9vORPpwXfH_4SpSnBi_gaf9cNXMb4POoFn1ftJBFyBebvArQXswhbzJ0mL4B3szk4ZNgZ2MA/p.png",
+    your_img_url: "https://i.imgur.com/YmJYAjF.jpg",
 };
 
 const dataabout = {
